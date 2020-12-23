9278a629dd55bda7e3c5eb2818503c8c7b172409
logicraysDev <lrtestuser2018@gmail.com>
2020-12-23 12:13:56 +0530

    Advance custom field example

--- acf-code-example.php
+++ acf-code-example.php
@@ -13,6 +13,7 @@ $variable = get_field('field_name');
 
 /*------------Using conditional statements--------------*/
 
+get_field returns false if (value == "" || value == null || value == false)
 <?php
 
 if(get_field('field_name'))
@@ -21,3 +22,31 @@ if(get_field('field_name'))
 }
 
 ?>
+
+/*-----------------Working with Array values------------------*/
+
+checkbox, select, relationship, repeater
+
+<?php
+
+$values = get_field('field_name');
+if($values)
+{
+	echo '<ul>';
+
+	foreach($values as $value)
+	{
+		echo '<li>' . $value . '</li>';
+	}
+
+	echo '</ul>';
+}
+
+// always good to see exactly what you are working with
+var_dump($values);
+
+?>
+
+/*---------------Working with Images – URL----------------------*/
+
+<img src="<?php the_field('image_test'); ?>" alt="" />
